4361d47f84fbcb102ef064b03ee56efb89c6ac30
senseiiii12 <Sasha.chepiga228@gmail.com>
2026-04-26 18:31:00 +0300

    Add Turbine dependency

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -32,6 +32,7 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
                 add("implementation", libs.findLibrary("kotlinx.coroutines.core").get())
                 add("implementation", libs.findLibrary("kotlinx.coroutines.android").get())
                 add("testImplementation", libs.findLibrary("kotlinx.coroutines.test").get())
+                add("testImplementation", libs.findLibrary("turbine").get())
                 add("testImplementation", libs.findLibrary("junit").get())
                 add("androidTestImplementation", libs.findLibrary("androidx.junit").get())
                 add("androidTestImplementation", libs.findLibrary("androidx.espresso.core").get())
