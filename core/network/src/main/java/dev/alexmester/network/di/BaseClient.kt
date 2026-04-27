ed186b7b5afac12bb8ee8980d03867d478b1d20f
senseiiii12 <Sasha.chepiga228@gmail.com>
2026-04-28 02:04:12 +0300

    add constants in NetworkModule

---
+++ core/network/src/main/java/dev/alexmester/network/di/BaseClient.kt
@@ -0,0 +1,54 @@
+package dev.alexmester.network.di
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.ANDROID
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object BaseClient {
+
+    fun createBaseClient() = HttpClient(Android) {
+        expectSuccess = true
+
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                isLenient = true
+                coerceInputValues = true
+            })
+        }
+
+        install(Logging) {
+            logger = Logger.ANDROID
+            level = LogLevel.HEADERS
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = 30_000
+            connectTimeoutMillis = 15_000
+            socketTimeoutMillis = 30_000
+        }
+
+        install(HttpRequestRetry) {
+            retryIf { _, response ->
+                response.status.value in 500..599
+            }
+            exponentialDelay(
+                base = 2.0,
+                maxDelayMs = 10_000,
+            )
+            maxRetries = 3
+        }
+    }
+
+    const val BASE_URL_WORLD_NEWS = "https://api.worldnewsapi.com/"
+    const val BASE_URL_TRANSLATE = "https://api.translateplus.io/"
+
+}
--- core/network/src/main/java/dev/alexmester/network/di/NetworkModule.kt
+++ core/network/src/main/java/dev/alexmester/network/di/NetworkModule.kt
@@ -1,73 +1,28 @@
 package dev.alexmester.network.di
 
 import dev.alexmester.network.BuildConfig
-import dev.alexmester.network.plugin.ApiKeyWorldNewsPlugin
+import dev.alexmester.network.di.BaseClient.BASE_URL_TRANSLATE
+import dev.alexmester.network.di.BaseClient.BASE_URL_WORLD_NEWS
+import dev.alexmester.network.di.BaseClient.createBaseClient
 import dev.alexmester.network.plugin.ApiKeyTranslatePlugin
+import dev.alexmester.network.plugin.ApiKeyWorldNewsPlugin
 import dev.alexmester.network.translate.TranslateApiService
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.DefaultRequest
-import io.ktor.client.plugins.HttpRequestRetry
-import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.ANDROID
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-fun createBaseClient() = HttpClient(Android) {
-    expectSuccess = true
-
-    install(ContentNegotiation) {
-        json(Json {
-            ignoreUnknownKeys = true
-            isLenient = true
-            coerceInputValues = true
-        })
-    }
-
-    install(Logging) {
-        logger = Logger.ANDROID
-        level = LogLevel.HEADERS
-    }
-
-    install(HttpTimeout) {
-        requestTimeoutMillis = 30_000
-        connectTimeoutMillis = 15_000
-        socketTimeoutMillis = 30_000
-    }
-
-    install(HttpRequestRetry) {
-        retryIf { _, response ->
-            response.status.value in 500..599
-        }
-        exponentialDelay(
-            base = 2.0,
-            maxDelayMs = 10_000,
-        )
-        maxRetries = 3
-    }
-}
-
-enum class Clients{
+enum class Clients {
     WORLD_NEWS,
     TRANSLATE,
 }
 
 val networkModule = module {
-
     single(named(Clients.WORLD_NEWS)) {
         createBaseClient().config {
-
             install(DefaultRequest) {
-                url("https://api.worldnewsapi.com/")
+                url(BASE_URL_WORLD_NEWS)
                 headers.append("Accept", "application/json")
             }
-
             install(ApiKeyWorldNewsPlugin) {
                 apiKey = BuildConfig.NEWS_API_KEY
             }
@@ -76,12 +31,10 @@ val networkModule = module {
 
     single(named(Clients.TRANSLATE)) {
         createBaseClient().config {
-
             install(DefaultRequest) {
-                url("https://api.translateplus.io/")
+                url(BASE_URL_TRANSLATE)
                 headers.append("Accept", "application/json")
             }
-
             install(ApiKeyTranslatePlugin) {
                 apiKey = BuildConfig.TRANSLATE_PLUS_API_KEY
             }
