adb11be6d525128b000a183706f2ec0a42478ed6
senseiiii12 <Sasha.chepiga228@gmail.com>
2026-05-06 18:21:10 +0300

    update strings.xml and warning colors

--- core/ui/src/main/java/dev/alexmester/ui/desing_system/LaskPalette.kt
+++ core/ui/src/main/java/dev/alexmester/ui/desing_system/LaskPalette.kt
@@ -33,8 +33,8 @@ object LaskPalette {
     val ErrorDark = Color(0xFFE45F6D)
     val ErrorLight = Color(0xFFFF3B45)
 
-    val WarningDark = Color(0xFFFDAC65)
-    val WarningLight = Color(0xFFFF9232)
+    val WarningDark = Color(0xFFFFCF9E)
+    val WarningLight = Color(0xFFFF8200)
 
     val InformativeDark = Color(0xFF416BD6)
     val InformativeLight = Color(0xFF2D5BD0)
